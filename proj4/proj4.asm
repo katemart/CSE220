936ada960baf157cb693bbd80dc2121c9e190a27
Kathy Martinez <katherynymartinez@gmail.com>
2019-11-23 20:38:43 -0500

    network packet processing simulation project

---
+++ proj4/proj4.asm
@@ -0,0 +1,36 @@
+# CSE 220 Programming Project #4
+# Katheryn Martinez Hernandez
+# ***REMOVED***
+# ***REMOVED***
+
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+
+.text
+
+compute_checksum:
+jr $ra
+
+compare_to:
+jr $ra
+
+packetize:
+jr $ra
+
+clear_queue:
+jr $ra
+
+enqueue:
+jr $ra
+
+dequeue:
+jr $ra
+
+assemble_message:
+jr $ra
+
+
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
+#################### DO NOT CREATE A .data SECTION ####################
