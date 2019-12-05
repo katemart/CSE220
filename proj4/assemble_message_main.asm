425bb3625b5f2b17989ecb71b5c471a4eac1da74
Kathy Martinez <katherynymartinez@gmail.com>
2019-12-04 20:24:18 -0500

    added functions to print queue and packet contents

--- proj4/assemble_message_main.asm
+++ proj4/assemble_message_main.asm
@@ -10,6 +10,18 @@ v0: .asciiz "v0: "
 v1: .asciiz "v1: "
 msg_str: .asciiz "msg: "
 
+version: .asciiz "Version: "
+msg_id: .asciiz "Msg ID #: "
+total_len: .asciiz "Total Length: "
+priority: .asciiz "Priority: "
+flags: .asciiz "Flags: "
+protocol: .asciiz "Protocol: "
+frag_off: .asciiz "Fragment Offset: "
+checksum: .asciiz "Checksum: "
+src_addr: .asciiz "Source Address: "
+des_addr: .asciiz "Dest Address: "
+payload: .asciiz "Payload: "
+
 all_packets:
 p154_32:
 .align 2
@@ -75,8 +87,216 @@ li $a0, '\n'
 li $v0, 11
 syscall
 
-# You will need to write your own code here to check the contents of the queue.
+la $a0, random_junk
+li $v0, 4
+syscall
 
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# You will need to write your own code here to check the contents of the queue.
+la $t0, queue
+lhu $t2, 2($t0)
+blez $t2, end_main
+li $t3, 4
+print_queue:
+lw $t1, 0($t0)
+move $a0, $t1
+li $v0, 34
+syscall
+li $a0, ' '
+li $v0, 11
+syscall
+syscall
+print_queue_loop:
+lw $t1, 4($t0)
+move $a0, $t1
+li $v0, 34
+syscall
+li $a0, ' '
+li $v0, 11
+syscall
+syscall
+addi $t0, $t0, 4
+addi $t2, $t2, -1
+bgtz $t2, print_queue_loop
+li $a0, '\n'
+li $v0, 11
+syscall
+
+################################### print packet ###################################
+la $t0, p154_0
+# first line
+lw $t1, 0($t0)
+andi $t2, $t1, 0xFFFF							# $t2 = total_len
+move $t8, $t2									# $t8 = total_len
+srl $t3, $t1, 16
+srl $t1, $t3, 12								# $t1 = version
+andi $t3, $t3, 0xFFF							# $t3 = msg_id
+la $a0, version
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t1
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, msg_id
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t3
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, total_len
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t2
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+# second line
+lw $t1, 4($t0)
+andi $t2, $t1, 0xFFF							# $t2 = frag_offset
+srl $t3, $t1, 12
+srl $t4, $t3, 10
+andi $t3, $t3, 0x3FF							# $t3 = protocol
+srl $t1, $t4, 2									# $t1 = priority
+andi $t4, $t4, 0x3								# $t4 = flags
+la $a0, priority
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t1
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, flags
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+syscall
+move $a0, $t4
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, protocol
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t3
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, frag_off
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+move $a0, $t2
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+# third line
+lw $t1, 8($t0)
+andi $t2, $t1, 0xFF								# $t2 = des_addr
+srl $t3, $t1, 8	
+srl $t1, $t3, 8									# $t1 = checksum
+andi $t3, $t3, 0xFF								# $t3 = src_addr
+la $a0, des_addr
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t2
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, src_addr
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+move $a0, $t3
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+la $a0, checksum
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+move $a0, $t1
+li $v0, 1
+syscall
+li $a0, '\n'
+li $v0, 11
+syscall
+# fourth line
+la $a0, payload
+li $v0, 4
+syscall
+li $a0, '\t'
+li $v0, 11
+syscall
+syscall
+addi $t8, $t8, -12								# $t8 = payload size = counter
+payload_loop:
+lbu $t1, 12($t0)								# $t1 = first char of payload
+move $a0, $t1
+li $v0, 11
+syscall
+addi $t0, $t0, 1								# go to next char of payload
+addi $t8, $t8, -1								# counter--
+bgtz $t8, payload_loop							# if counter > 0, loop again
+###################################################################################
+
+end_main:
 li $v0, 10
 syscall
 
